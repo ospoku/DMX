ea583a1dfedf1efb57599c95eb0f31c3fb9dfdea
oseipoku <ospoku@gmail.com>
2024-07-15 07:22:50 +0000

    avatar initials

--- DMX/Models/AppUser.cs
+++ DMX/Models/AppUser.cs
@@ -20,12 +20,7 @@ namespace DMX.Models
                     + Surname;
             }
         }
-public string Initials { get {return Fullname.Split(' ').Where(part => !string.IsNullOrEmpty(part))
-                               .Select(part => part[0])
-                               .Take(2)
-                               .ToArray().ToString();
-            }
-        }
+
 
     
         public bool IsDeleted { get; set; }

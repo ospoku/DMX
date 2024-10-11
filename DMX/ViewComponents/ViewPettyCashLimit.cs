c708291a9e110e9273f4f8f9d486834e45795c17
oseipoku <ospoku@gmail.com>
2024-10-11 18:28:33 +0000

    updating pettycash limit

--- DMX/ViewComponents/ViewPettyCashLimit.cs
+++ DMX/ViewComponents/ViewPettyCashLimit.cs
@@ -1,4 +1,6 @@
-﻿using DMX.Models;
+﻿using DMX.Data;
+using DMX.DataProtection;
+using DMX.Models;
 using DMX.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -9,17 +11,24 @@ namespace DMX.ViewComponents
     public class ViewPettyCashLimit : ViewComponent
     {
         public readonly UserManager<AppUser> usm;
+        public readonly XContext  dcx;
+     
 
-        public ViewPettyCashLimit(UserManager<AppUser> userManager)
+        public ViewPettyCashLimit(UserManager<AppUser> userManager, XContext context)
         {
             usm = userManager;
-
+            dcx = context;
+            
         }
         public IViewComponentResult Invoke()
         {
-           ViewPettyCashLimitVM viewPettyCashLimit = new()
+            //PettyCashLimit limit = new PettyCashLimit();
+
+            //var limitToUpdate = dcx.PettyCashLimits.FirstOrDefault(p => p.PettyCashLimitId ==limit.PettyCashLimitId).PettyCashLimitId;
+
+            ViewPettyCashLimitVM viewPettyCashLimit = new()
             {
-                
+                //Amount=limitToUpdate,
                
         };
 

89a55706730ec7d22920f22f0576b3d3c30757b1
oseipoku <ospoku@gmail.com>
2025-10-21 17:58:09 +0100

    fixed all dialogs

--- DMX/wwwroot/ospapps/choices.js
+++ DMX/wwwroot/ospapps/choices.js
@@ -1,2 +1,15 @@
 ﻿document.addEventListener('DOMContentLoaded', () =>
-{ const selects = document.querySelectorAll('select.dmx-choices'); selects.forEach(select => { const choices = new Choices(select, { searchEnabled: true, itemSelectText: '', shouldSort: false, }); }); });    
+{
+    const selects = document.querySelectorAll('select.dmx-choices');
+    selects.forEach(select => {
+        const choices = new Choices(select,
+            {
+                removeItemButton: true,
+                searchEnabled: true,
+                itemSelectText: '',
+                shouldSort: false,
+                placeholder: true,
+                placeholderValue: 'Select Approver(s)',
+            });
+    });
+});    

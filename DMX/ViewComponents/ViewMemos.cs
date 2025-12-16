32c0862a5eead6ea3a9d68f80507ef02063314e9
ospoku <ospoku@gmail.com>
2025-12-16 03:18:40 +0000

    fixing canPrint in VC

--- DMX/ViewComponents/ViewMemos.cs
+++ DMX/ViewComponents/ViewMemos.cs
@@ -1,41 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+
 using DMX.Data;
 using DMX.ViewModels;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using DMX.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DMX.ViewComponents
 {
-    public class ViewMemos(XContext dContext, UserManager<AppUser>userManager) : ViewComponent
+    public class ViewMemos(XContext dContext, UserManager<AppUser> userManager, IAuthorizationService authorization) : ViewComponent
     {
         public readonly XContext dcx = dContext;
 
         private readonly UserManager<AppUser> usm = userManager;
-
-        public async Task<IViewComponentResult>InvokeAsync()
+        public readonly IAuthorizationService auth = authorization;
+        public async Task<IViewComponentResult> InvokeAsync()
         {
-            var user = (await usm.GetUserAsync(HttpContext.User)).Id;
-            var memoList = dcx.MemoAssignments.Where(a=>a.AppUser.Id==user||a.Memo.CreatedBy==user & a.Memo.IsDeleted==false).Select(a => new ViewMemosVM
-            {
-                PublicId = a.Memo.PublicId,
+            var user = (await usm.GetUserAsync(HttpContext.User));
+            var memoList = dcx.MemoAssignments.Where(a => a.AppUser.Id == user.Id || a.Memo.CreatedBy == user.Id & a.Memo.IsDeleted == false).ToList();
 
-                Content = a.Memo.Content,
-                ReferenceNumber=a.Memo.ReferenceId,
-             
-                Assignees = (from u in usm.Users where u.Id == a.UserId select u.UserName).ToList(),
-                Title = a.Memo.Title,
-                
-                CreatedDate = a.CreatedDate,
-                CreatedBy=a.CreatedBy
-
-            }).OrderByDescending(a=>a.CreatedDate).ToList();
             foreach (var memo in memoList)
             {
+                var canEdit = await auth.AuthorizeAsync(HttpContext.User, memo, "MemoOwnerPolicy");
+
+                var canPrint = HttpContext.User.HasClaim("Permission", "Permission.MemoPrint");
                 var senderUser = await usm.FindByIdAsync(memo.CreatedBy);
-                memo.Sender = senderUser?.Fullname;
+                List<ViewMemosVM>memoVm = new ViewMemosVM()
+                {
+                    PublicId = memo.PublicId,
+
+                    Content = memo.Memo.Content,
+                    ReferenceNumber = memo.Memo.ReferenceId,
+
+                    Assignees = (from u in usm.Users where u.Id == memo.UserId select u.UserName).ToList(),
+                    Title = memo.Memo.Title,
+
+                    CreatedDate = memo.CreatedDate,
+                    CreatedBy = memo.CreatedBy
+                };
+            
+
+            return View(memoVmm).ToList());
             }
-            return View(memoList);
         }
-    }
-}
+    } }
